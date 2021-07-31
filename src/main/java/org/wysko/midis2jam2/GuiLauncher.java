59211873473e7c3f0d85edf2fb2b1a51381a2e0f
Jacob Wysko <jacob@wysko.org>
2021-07-30 20:08:33 -0400

    Fix translation
    * Rename file
    
    * Convert accent characters to basic latin

--- src/main/java/org/wysko/midis2jam2/GuiLauncher.java
+++ src/main/java/org/wysko/midis2jam2/GuiLauncher.java
@@ -62,8 +62,8 @@ public class GuiLauncher extends JFrame {
 	
 	static {
 		supportedLocales.put("English", "en");
-		supportedLocales.put("Español", "es");
-		supportedLocales.put("Français", "fr");
+		supportedLocales.put("Espa\u00F1ol", "es");
+		supportedLocales.put("Fran\u00E7ais", "fr");
 		supportedLocales.put("UwU", "uwu");
 	}
 	
