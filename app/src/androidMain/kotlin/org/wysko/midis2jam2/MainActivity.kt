895f1e5ae094423e3c1cbb61e7d3843cd8d33538
Jacob Wysko <jacob@wysko.org>
2025-06-26 21:14:41 -0400

    Cleanup MainActivity.kt

--- app/src/androidMain/kotlin/org/wysko/midis2jam2/MainActivity.kt
+++ app/src/androidMain/kotlin/org/wysko/midis2jam2/MainActivity.kt
@@ -18,53 +18,16 @@
 package org.wysko.midis2jam2
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.lifecycle.lifecycleScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import org.koin.core.context.startKoin
-import org.koin.core.error.KoinApplicationAlreadyStartedException
-import org.wysko.midis2jam2.di.applicationModule
-import org.wysko.midis2jam2.di.midiSystemModule
-import org.wysko.midis2jam2.di.systemModule
-import org.wysko.midis2jam2.di.uiModule
-import org.wysko.midis2jam2.util.logger
-import java.io.IOException
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
-
         setContent {
             App()
-        }
-    }
-
-    @Throws(IOException::class)
-    private fun copyAssetToTmpFile(fileName: String): String {
-        assets.open(fileName).use { `is` ->
-            val tempFileName = "tmp_$fileName"
-            openFileOutput(tempFileName, MODE_PRIVATE).use { fos ->
-                var bytes_read: Int
-                val buffer = ByteArray(4096)
-                while ((`is`.read(buffer).also { bytes_read = it }) != -1) {
-                    fos.write(buffer, 0, bytes_read)
-                }
-            }
-            Log.d("MainActivity", "Copied asset to temp file: $tempFileName")
-            return "$filesDir/$tempFileName"
-        }
-    }
-
-    external fun helloWorldFluidSynth(soundfontPath: String)
-
-    companion object {
-        init {
-            System.loadLibrary("native-lib")
         }
     }
 }
